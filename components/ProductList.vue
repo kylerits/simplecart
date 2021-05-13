694f84aa7fbe3ff8e1d95e28976fa89e63c00957
Kyle Rodgers <krr.webdev@gmail.com>
2021-05-13 19:21:37 -0400

    building out a product list

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
-    <ul class="relative block">
+    <div class="relative block">
       <!-- Products Loop -->
-      <li v-for="(product, index) in products" :key="'product-'+index" class="block list-style-none"></li>
-    </ul>
+      <div
+        v-for="(product, index) in products"
+        :key="'product-' + index"
+        class="relative max-w-md"
+      >
+        <div class="flex product-inner">
+          <div class="product-info">
+            <div class="prose">
+              <h3>{{ product.productName }}</h3>
+              <p>{{ product.description }}</p>
+            </div>
+            <div class="flex items-center">
+              <div>${{ product.price }}</div>
+              <div>
+                <button
+                  id="addToCart"
+                  class="px-3 py-1 text-white duration-200 bg-green-500 rounded  hover:bg-green-600"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
