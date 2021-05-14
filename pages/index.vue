a64493b729512739bde0f0cfd0d7093caf932d47
Kyle Rodgers <krr.webdev@gmail.com>
2021-05-13 20:51:14 -0400

    adding header

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="container">
-    <div class="flex -mx-6">
+  <div class="container py-12">
+    <div class="text-center prose pb-12 mx-auto">
+      <h1>Simple Cart</h1>
+    </div>
+    <div class="flex -mx-6 py-12">
       <div class="relative w-1/2 px-6">
         <lazy-product-list />
       </div>
